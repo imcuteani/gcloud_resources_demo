37f0a383cec4ba60108817219e5feec18a874188
imcuteani <anindita.basak@live.com>
2022-07-17 20:18:20 +0530

    updated gcp terraform cloud ws

--- Kubernetes/GKE_Node_Pool/providers.tf
+++ Kubernetes/GKE_Node_Pool/providers.tf
@@ -2,3 +2,13 @@ provider "google" {
   project = var.project
   region  = var.region
 }
+
+terraform {
+  cloud {
+    organization = "Woodgroove"
+
+    workspaces {
+      name = "gcp_github_actions"
+    }
+  }
+}
